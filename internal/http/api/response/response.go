e053e5f2d0b453225065df7b76d1020ce4ba7309
urusofam <urusofam@gmail.com>
2025-05-26 16:36:04 +0300

    add useful ValidationError

--- internal/http/api/response/response.go
+++ internal/http/api/response/response.go
@@ -1,5 +1,11 @@
 package response
 
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"strings"
+)
+
 type Response struct {
 	Status string `json:"status"`
 	Error  string `json:"error,omitempty"`
@@ -15,4 +21,21 @@ func OK() Response {
 }
 func Error(msg string) Response {
 	return Response{Status: StatusError, Error: msg}
+}
+
+func ValidationError(errs validator.ValidationErrors) Response {
+	var errMsgs []string
+
+	for _, err := range errs {
+		switch err.ActualTag() {
+		case "required":
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is required", err.Field()))
+		case "url":
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is invalid URL", err.Field()))
+		default:
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is invalid value", err.Field()))
+		}
+	}
+
+	return Response{Status: StatusError, Error: strings.Join(errMsgs, ", ")}
 }
